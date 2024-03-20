61328acd0acf1a9a06af1c25bde90a9c962de239
HgThinker <nguyenhoanggia143@gmail.com>
2024-03-20 09:04:45 +0700

    create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+# with open('LICENSE', encoding='utf-8') as f:
+#     license = f.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    reqs = f.readlines()[1:]
+
+setup(
+    name = 'timeanddate_crawler',
+    version='0.1.0',
+    description='Timeanddate.com data crawler',
+    long_description='readme',
+    python_requires='>=3.9',
+    package_dir={"": "src"},
+    packages=find_packages(exclude=('data')),
+    # install_requires=reqs.strip().split('\n'),
+    install_requires=reqs,
+)
