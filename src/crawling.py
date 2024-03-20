0c31fe524bd9eb03c32c81b42068ef1fcc6e8d70
HgThinker <nguyenhoanggia143@gmail.com>
2024-03-20 09:22:21 +0700

    remove chrome_driver_manager

--- src/crawling.py
+++ src/crawling.py
@@ -3,7 +3,6 @@ import PIL
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 import requests
 import PIL
