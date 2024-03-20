3d2dec4e5326eaf44c074d3e7d4d11fab4dfc307
HgThinker <nguyenhoanggia143@gmail.com>
2024-03-20 13:32:43 +0700

    Problem: Increasing reponsing time Solution: rerequest url

--- src/crawling.py
+++ src/crawling.py
@@ -29,54 +29,62 @@ dataset.loc[len(dataset)]=['','','', '°F', '', 'mph','', '%', 'Hg', 'mi']
 
 #START CRAWLING
 # select MONTH dropdown
-time.sleep(10)
-ele_month = driver.find_element(By.ID,'month')
-# ele_month = wait.until(EC.visibility_of_all_elements_located((By.ID,'month')))
-# ele_month=wait.until(EC.visibility_of_element_located(ele_month))
-select_month = Select(ele_month)
-for month in select_month.options[3:]:
-  time.sleep(100)
-  month.click 
-  month_name = month.text
-  print("Selected month:", month_name)
-  # wait.until(EC.visibility_of_any_elements_located((By.NAME,month.text)))
-  select_month.select_by_visible_text(month_name) # Choose a  SPECIFIC month
-  # Click on that month
-  # SELECT DAY DROPDOWN
-  ele_day = driver.find_element(By.ID,'wt-his-select')
-  # ele_day = wait.until(EC.visibility_.textof_any_elements_located((By.ID,'wt-his-select')))
-  select_day = Select(ele_day)
-  # Take all data of a day
-  for day in select_day.options[27:]:
-    day_name = day.text
-    print("Selected day:", day_name)
-    #Find data of hours in day
-    select_day.select_by_visible_text(day_name)
-    elements = wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, 'tr')))
-    # elements=driver.find_elements(By.TAG_NAME,"tr")#Find all hours
-    count_row=7
-    for element_details in elements[7:-1]:#Loop through hours
-      print(element_details.text)
-      children = element_details.find_elements(By.XPATH, "./*")
-      # children = wait.until(EC.visibility_of_all_elements_located(By.XPATH, "./*"))
-      detail_list = [month_name.split()[1], day_name.split(', ')[0]]
-      count=0
-      if count_row==7:# if it is the first hours, It is a special case
-        detail_list.append(children[0].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%','')[:8])# cut off the first data
-        for children_index in range(1,len(children)):#Get the left over data
-          if children_index == 5:
-            detail_list.append(children[children_index].find_element(By.XPATH, "./*").get_attribute('title').split()[3])
-          else:
-            detail_list.append(children[children_index].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%',''))
+current_month_index=1
+while True:
+  try:
+    time.sleep(10)
+    print("current_month_index: ", current_month_index)
+    ele_month = driver.find_element(By.ID,'month')
+    # ele_month = wait.until(EC.visibility_of_all_elements_located((By.ID,'month')))
+    # ele_month=wait.until(EC.visibility_of_element_located(ele_month))
+    select_month = Select(ele_month)
+    month =  select_month.options[current_month_index]
+    time.sleep(10)
+    month.click 
+    month_name = month.text
+    print("Selected month:", month_name)
+    # wait.until(EC.visibility_of_any_elements_located((By.NAME,month.text)))
+    select_month.select_by_visible_text(month_name) # Choose a  SPECIFIC month
+    # Click on that month
+    # SELECT DAY DROPDOWN
+    ele_day = driver.find_element(By.ID,'wt-his-select')
+    # ele_day = wait.until(EC.visibility_.textof_any_elements_located((By.ID,'wt-his-select')))
+    select_day = Select(ele_day)
+    # Take all data of a day
+    for day in select_day.options:
+      time.sleep(1)
+      day_name = day.text
+      print("Selected day:", day_name)
+      #Find data of hours in day
+      select_day.select_by_visible_text(day_name)
+      elements = wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, 'tr')))
+      # elements=driver.find_elements(By.TAG_NAME,"tr")#Find all hours
+      count_row=7
+      for element_details in elements[7:-1]:#Loop through hours
+        print(element_details.text)
+        children = element_details.find_elements(By.XPATH, "./*")
+        # children = wait.until(EC.visibility_of_all_elements_located(By.XPATH, "./*"))
+        detail_list = [month_name.split()[1], day_name.split(', ')[0]]
+        count=0
+        if count_row==7:# if it is the first hours, It is a special case
+          detail_list.append(children[0].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%','')[:8])# cut off the first data
+          for children_index in range(1,len(children)):#Get the left over data
+            if children_index == 5:
+              detail_list.append(children[children_index].find_element(By.XPATH, "./*").get_attribute('title').split()[3])
+            else:
+              detail_list.append(children[children_index].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%',''))
 
-      else:
-        for children_index in range(0,len(children)):#Get all data of hour
-          if children_index == 5:
-            detail_list.append(children[children_index].find_element(By.XPATH, "./*").get_attribute('title').split()[3])
-          else:
-            detail_list.append(children[children_index].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%',''))
-      detail_list = [x for x in detail_list if x]
-      dataset.loc[len(dataset)] = detail_list#Add new row to dataset
-      count_row+=1
-      if count_row % 5 ==0:
-        dataset.to_csv('/content/drive/MyDrive/Reeco/timeanddate_dataset.csv')
+        else:
+          for children_index in range(0,len(children)):#Get all data of hour
+            if children_index == 5:
+              detail_list.append(children[children_index].find_element(By.XPATH, "./*").get_attribute('title').split()[3])
+            else:
+              detail_list.append(children[children_index].text.replace('°F','').replace('mph','').replace('\"Hg','').replace('%',''))
+        detail_list = [x for x in detail_list if x]
+        dataset.loc[len(dataset)] = detail_list#Add new row to dataset
+        count_row+=1
+        if count_row % 5 ==0:
+          dataset.to_csv('/content/drive/MyDrive/Reeco/timeanddate_dataset.csv')
+    current_month_index+=1
+  except:
+    driver.get(web_url)
